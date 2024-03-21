70012222249ee80b3adf671e7916ea9b7960c279
Alexis de Lattre <alexis.delattre@akretion.com>
2024-03-21 16:03:34 +0100

    [MIG] project_viewer to v14

--- project_viewer/__manifest__.py
+++ project_viewer/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2013-2019 Akretion (http://www.akretion.com)
+# Copyright 2013-2024 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Project Viewer',
     'summary': """Adds a group 'Project Viewer'""",
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Project Management',
     'license': 'AGPL-3',
     'description': """
@@ -17,7 +17,7 @@ This module adds a group *Project Viewer* in the *Project* application. This gro
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/akretion/odoo-viewer-groups',
     'depends': ['project'],
     'data': [
         'security/project_security.xml',
